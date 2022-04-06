eb346f5f2eb61616d7f91060db1baa5d46d1cfa6
chrisGrando <christianjulio2010@gmail.com>
2022-04-06 15:31:30 -0300

    Database insert script (50%)

---
+++ dtInsert.sql
@@ -0,0 +1,46 @@
+--Inserção de tuplas
+--Tuplas de Usuários
+INSERT INTO usuario(email, nome_real, nome_usuario, senha) VALUES
+  ('chris.grando@hotmail.com', 'Christian Grando', 'kris', 'bd2021.2'),
+  ('marisa.kirisame@big.com.jp', 'Marisa Kirisame', 'MaLice', 'magicannon11'),
+  ('reimu.hakurei@big.com.jp', 'Reimu Hakurei', 'Maiden', 'youkai-sealing'),
+  ('michelliz@gmail.com.br', 'Michelle de Liz', 'MiSilva', 'ep@gr1'),
+  ('neusaantunes@yahoo.com', 'Neusa Antunes', 'chiquinha', 'crav0-r0sa'),
+  ('susie2018@outlook.com.br', 'Susie Pavan', 'susie.axe', 'Lan-C3r'),
+  ('emily.wilson@estudante.uffs.edu.br', 'Emily Wilson', 'EmiWil', 'PressC2Win'),
+  ('tifannyblues@gmail.com', 'Tifanny Blues', 'Tifa19', 'blue_eyesss'),
+  ('george2010@yahoo.com.br', 'George White', 'little.george', 'MyWh1teS0ul'),
+  ('ernesto.nesto@estudante.uffs.edu.br', 'Ernesto Thomas Nesto', 'ThomasNesto', 'l0st1nMind');
+
+--Inserção de Anotações
+INSERT INTO anotacoes(id, texto) VALUES
+  (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ornare aliquet nunc, non cursus augue consectetur eu. Nunc et risus lobortis, bibendum leo a, pellentesque justo. Aenean accumsan mi sed eleifend efficitur. Nullam suscipit neque vel quam auctor facilisis. Nam tristique finibus tellus eget placerat. Quisque consequat libero sit amet lectus finibus, ut posuere mi posuere. Ut dignissim felis nisl, in fringilla nibh ultricies hendrerit. Proin at porttitor eros. Nam vitae ipsum in justo ultricies consequat ac a sapien. Maecenas vulputate, metus in cursus laoreet, est urna posuere metus, id varius sapien ante varius purus. Aenean iaculis imperdiet varius. Duis interdum purus eu mauris feugiat, eget sollicitudin turpis accumsan. Praesent et ipsum ac lectus faucibus accumsan. Sed scelerisque erat risus, facilisis feugiat felis tempus ultricies. Morbi fringilla elementum elit, sit amet faucibus felis viverra sed.'),
+  (2, 'Aliquam ullamcorper, lacus at consequat feugiat, justo elit faucibus felis, et sagittis magna nunc vitae enim. Duis sem leo, mattis non ipsum eget, viverra vulputate magna. Ut lobortis dignissim nibh et vestibulum. Phasellus sagittis felis vel eros blandit, eu sagittis leo pharetra. Integer interdum vel purus in volutpat. Donec bibendum sed ipsum in pulvinar. Duis vestibulum pharetra turpis. Nullam et ante eu velit dignissim sollicitudin quis ut quam. Nullam non bibendum nisl. Donec rhoncus ullamcorper quam, consequat consectetur leo mollis a. Etiam tincidunt lectus cursus leo aliquet mattis vitae id ex. Nunc at sollicitudin turpis, sit amet rhoncus eros. Sed gravida eleifend nulla. Quisque non felis in lorem dapibus faucibus. Proin cursus rutrum enim, ac venenatis metus.'),
+  (3, 'Etiam commodo feugiat urna quis blandit. Mauris lacinia porta nibh quis aliquet. Sed tempor, nulla eu efficitur condimentum, ligula libero cursus orci, at faucibus nisi enim eget ante. Vivamus bibendum, mauris ac ornare vehicula, mauris tortor suscipit metus, id porttitor sem lorem nec justo. Vivamus sed ligula gravida, pretium tortor in, venenatis ipsum. Praesent faucibus metus pulvinar diam pellentesque, finibus sollicitudin risus scelerisque. Aenean pulvinar lectus semper quam rutrum eleifend ac eget sem.'),
+  (4, 'Duis non erat lacinia, dignissim enim quis, commodo lacus. In id leo a massa placerat viverra. Nulla ut nulla non nulla efficitur finibus. Fusce pretium nulla nunc, non cursus ante varius nec. Aliquam erat volutpat. Aenean facilisis quis odio tincidunt viverra. Morbi venenatis est sed nulla suscipit blandit. Integer lacus orci, ornare at tempor at, egestas vel nunc. Fusce tincidunt mollis lacus sed finibus. Nulla a feugiat urna. Aliquam id malesuada leo.'),
+  (5, 'Nulla volutpat pretium urna, a tristique justo ultrices in. Sed blandit mi risus, id interdum dolor auctor quis. Donec eleifend semper odio, at rutrum lorem. Nam et viverra est, quis euismod elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur et risus eget arcu blandit sagittis. Cras gravida quis justo sed eleifend. Fusce pellentesque, orci et placerat dictum, elit leo faucibus mauris, vel tincidunt neque metus sit amet arcu. Sed sodales suscipit libero, eget elementum urna accumsan eu. Vivamus quis faucibus diam, id vulputate ante.'),
+  (6, 'Praesent faucibus magna in lorem molestie vulputate. Cras nec leo est. Mauris tempus lorem quis felis finibus tempus. Quisque quis semper quam. Integer at quam mi. Ut sit amet condimentum orci, fermentum consequat neque. Nam maximus at ligula a feugiat. Sed accumsan ornare faucibus. Sed rhoncus lacus in ipsum vestibulum, at iaculis ante porta. Sed hendrerit ante nec augue malesuada posuere.'),
+  (7, 'Aliquam nec mauris vitae diam condimentum tincidunt tincidunt vehicula augue. Nam rutrum consectetur metus, vitae elementum turpis pellentesque et. Proin sagittis, velit sed mattis tincidunt, magna leo lacinia quam, ut tristique felis augue sit amet sem. Nam sed quam consectetur, imperdiet arcu eget, imperdiet ex. Sed efficitur augue sed elementum fermentum. Proin ac pellentesque nibh, ut rhoncus enim. Fusce in justo nulla. Vestibulum quis magna id odio eleifend sagittis. Pellentesque rutrum felis urna, in accumsan eros laoreet ac.'),
+  (8, 'Nam ornare lorem commodo mi pretium finibus. Nulla rhoncus tempor feugiat. Vestibulum commodo faucibus massa sit amet blandit. Nulla facilisi. Maecenas non vestibulum mi. Suspendisse efficitur dictum velit, a venenatis arcu interdum eu. Donec sodales varius tortor, vitae dapibus urna dapibus in. Donec mattis nisi ac nulla suscipit maximus. Nullam ornare enim ac pulvinar volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus.'),
+  (9, 'In orci enim, pharetra ac ullamcorper iaculis, congue eget libero. Sed dui orci, iaculis nec turpis sit amet, bibendum volutpat libero. Cras ullamcorper tellus eu urna vehicula volutpat sed at ipsum. Suspendisse potenti. Proin sagittis malesuada arcu vitae vulputate. Fusce pulvinar magna nunc, eget lacinia dolor porta ut. Suspendisse potenti. Nunc feugiat dolor ut mollis vehicula. Etiam ultricies malesuada ipsum, congue venenatis nibh sagittis vel. Aenean sodales diam nec turpis fermentum accumsan. Donec cursus orci magna, quis sollicitudin lectus laoreet et. Fusce hendrerit, ligula et tempor blandit, orci mauris cursus nibh, eget congue magna magna vel nisl. Nam ante dolor, ultricies nec tellus at, fringilla aliquet dolor. Vestibulum dictum faucibus laoreet. Praesent sit amet laoreet est.'),
+  (10, 'Vestibulum at diam luctus purus posuere consequat in at urna. Integer vitae rutrum eros, ac egestas magna. Donec porttitor enim vitae odio laoreet, vel mollis leo dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed pretium, mauris non ultricies pharetra, dui odio malesuada nunc, et interdum augue nisl finibus est. Curabitur metus odio, consectetur at nulla non, volutpat dapibus metus. Integer at arcu a dolor laoreet laoreet in a diam. Nullam mollis, metus nec rutrum dignissim, est eros tempor massa, accumsan laoreet mauris ante in neque. Nullam lacinia orci vitae lacus dignissim, sit amet lobortis leo laoreet. In quis est velit. Fusce blandit arcu non metus ornare porttitor. Aenean a venenatis nulla.');
+
+--Inserção da relação "Escrever"
+INSERT INTO escrever(email, id) VALUES
+  ('marisa.kirisame@big.com.jp', 1),
+  ('chris.grando@hotmail.com', 1),
+  ('susie2018@outlook.com.br', 3),
+  ('chris.grando@hotmail.com', 2),
+  ('reimu.hakurei@big.com.jp', 3),
+  ('emily.wilson@estudante.uffs.edu.br', 9),
+  ('ernesto.nesto@estudante.uffs.edu.br', 5),
+  ('emily.wilson@estudante.uffs.edu.br', 5),
+  ('michelliz@gmail.com.br', 10),
+  ('reimu.hakurei@big.com.jp', 4),
+  ('neusaantunes@yahoo.com', 6),
+  ('michelliz@gmail.com.br', 7),
+  ('chris.grando@hotmail.com', 8),
+  ('neusaantunes@yahoo.com', 9),
+  ('tifannyblues@gmail.com', 10);
+
+--Inserção de Eventos
